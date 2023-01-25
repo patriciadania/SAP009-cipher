3a2d41db7d1e7918cd07e1cf0e2ca9c88fc7471b
patriciadania <patricia.adania@hotmail.com>
2023-01-25 16:06:23 -0300

    https://github.com/patriciadania/SAP009-cipher.git

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
-const cipher = {
-  // ...
-};
+
+ 
+const cipher = {encode, decode};
+
+function encode(textoCifrar, offset){
+let mensagem = "";
+for (let i =0; i < textoCifrar.length;i++){
+let executa = ((textoCifrar.charCodeAt(i) -32 + offset) % 254) +32;
+
+mensagem += String.fromCharCode(executa);
+
+}
+return mensagem;
+}
+function decode(textoCifrado, offset){
+  let mensagem = "";
+  for (let i =0; i < textoCifrado.length;i++){
+  let executa = ((textoCifrado.charCodeAt(i) -32 - offset) % 254) +32;
+  
+  mensagem += String.fromCharCode(executa);
+  
+  }
+  return mensagem;
+  }
+  
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+
+function textoMaiusculo(event){
+    const texto = event.target.value
+    event.target.value
+    event.target.value = texto.toUpper.case();
+}
+
+function cifrar(){
+    const textoCifrar= document.querySelector("#TextoParaCriptografar").value;
+    const resultado = cipher.encode(textoCifrar, 5);
+    document.querySelector("#TextoCriptografado").innerText = resultado;
+};
+
+const inputCifrar = document.querySelector("#cifrar");
+inputCifrar.addEventListener("click",cifrar);
+
+function decifrar(){
+    const textoCifrado= document.querySelector("#TextoParaDescriptografar").value;
+    const traduz = cipher.decode(textoCifrado, 5);
+    document.querySelector("#TextoDescriptografado").innerText = traduz;
+};
+const inputDecifrar = document.querySelector("#decifrar");
+inputDecifrar.addEventListener("click",decifrar);
