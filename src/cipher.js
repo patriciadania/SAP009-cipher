ed82fa8d0654c487cc374ad407456a1cec9a68ef
patriciadania <patricia.adania@hotmail.com>
2023-01-26 17:41:58 -0300

    teste1

--- src/cipher.js
+++ src/cipher.js
@@ -3,25 +3,24 @@
 const cipher = {encode, decode};
 
 function encode(textoCifrar, offset){
-let mensagem = "";
-for (let i =0; i < textoCifrar.length;i++){
-let executa = ((textoCifrar.charCodeAt(i) -32 + offset) % 254) +32;
+  let mensagem = "";
+  for (let i =0; i < textoCifrar.length;i++){
+    let executa = ((textoCifrar.charCodeAt(i) -32 + offset) % 254) +32;
 
-mensagem += String.fromCharCode(executa);
+    mensagem += String.fromCharCode(executa);
 
-}
-return mensagem;
+  }
+  return mensagem;
 }
 function decode(textoCifrado, offset){
   let mensagem = "";
   for (let i =0; i < textoCifrado.length;i++){
-  let executa = ((textoCifrado.charCodeAt(i) -32 - offset) % 254) +32;
-  
-  mensagem += String.fromCharCode(executa);
+    let executa = ((textoCifrado.charCodeAt(i) -32 - offset) % 254) +32;
+    mensagem += String.fromCharCode(executa);
   
   }
   return mensagem;
-  }
+}
   
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import cipher from './cipher.js';
 
 
-
-function textoMaiusculo(event){
-    const texto = event.target.value
-    event.target.value
-    event.target.value = texto.toUpper.case();
-}
-
 function cifrar(){
-    const textoCifrar= document.querySelector("#TextoParaCriptografar").value;
-    const resultado = cipher.encode(textoCifrar, 5);
-    document.querySelector("#TextoCriptografado").innerText = resultado;
-};
+  const textoCifrar= document.querySelector("#TextoParaCriptografar").value;
+  const resultado = cipher.encode(textoCifrar, 3);
+  document.querySelector("#TextoCriptografado").innerText = resultado;
+}
 
 const inputCifrar = document.querySelector("#cifrar");
 inputCifrar.addEventListener("click",cifrar);
 
 function decifrar(){
-    const textoCifrado= document.querySelector("#TextoParaDescriptografar").value;
-    const traduz = cipher.decode(textoCifrado, 5);
-    document.querySelector("#TextoDescriptografado").innerText = traduz;
-};
+  const textoCifrado= document.querySelector("#TextoParaDescriptografar").value;
+  const traduz = cipher.decode(textoCifrado, 3);
+  document.querySelector("#TextoDescriptografado").innerText = traduz;
+}
 const inputDecifrar = document.querySelector("#decifrar");
 inputDecifrar.addEventListener("click",decifrar);
